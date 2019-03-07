5a45e7aca78085277edb615344e341b9be2dd079
AlexForJava <hqq@ukr.net>
2019-03-07 22:03:55 +0200

    first commit

---
+++ src/com/gmail/chernii/oleksii/Calculator.java
@@ -0,0 +1,9 @@
+package com.gmail.chernii.oleksii;
+
+/**
+ * Created by Space on 07.03.2019.
+ */
+@FunctionalInterface
+public interface Calculator {
+    double calculate(int first, int second, char action);
+}
---
+++ src/com/gmail/chernii/oleksii/Main.java
@@ -0,0 +1,32 @@
+package com.gmail.chernii.oleksii;
+
+import java.util.Scanner;
+
+/**
+ * Created by Space on 07.03.2019.
+ */
+public class Main {
+    private static Calculator calculator = (first, second, action) ->
+            (action == '+') ? (first + second) :
+                    (action == '*') ? (first * second) :
+                            (action == '-') ? (first - second) :
+                                    (action == '$' && second != 0) ? (Math.pow(first, 1 / second)) :
+                                            (action == '^') ? (Math.pow(first, second)) :
+                                                    (action == '#' && first != 0) ? ((first + second) / first * 1.0 + 117.) : 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean stop = false;
+        while (!stop) {
+            System.out.println("Enter first number");
+            int first = Integer.valueOf(sc.nextLine());
+            System.out.println("Enter second number");
+            int second = Integer.valueOf(sc.nextLine());
+            System.out.println("Enter action");
+            char action = sc.nextLine().charAt(0);
+            System.out.println("answer = " + calculator.calculate(first, second, action));
+            System.out.println("Stop? - Yes/No");
+            stop = (sc.nextLine().equalsIgnoreCase("yes")) ? true : false;
+        }
+    }
+}
